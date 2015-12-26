431eaa297986e65a87f965cd65ed4e6523ac3665
Silnyagin Yuri <sgloin@yandex.ru>
2015-12-26 20:58:42 +0900

    refactoring

--- src/Game.hs
+++ src/Game.hs
@@ -1,6 +1,5 @@
 module Game where
 
-import Graphics.Gloss.Interface.Pure.Game
 
 -- state of pong game
 data PongGame = Game
@@ -149,15 +148,3 @@ wallBounce game = game { ballVelocity = (vx, vy') }
             -- Do nothing. Return the old velocity.
             vy
 
-keyboardFunction :: Event -> PongGame -> PongGame
-keyboardFunction (EventKey (SpecialKey KeySpace) _ _ _) game = game { player1Paddle = (p1X, p1Y), player2Paddle = (p2X, p2Y), ballLocation = (ballX, ballY) }
-
-keyboardFunction (EventKey (SpecialKey KeyUp) _ _ _) game = if (player1Up game == False) then game { player1Up = True, player1Down = False } else game { player1Up = False } --game { player1Paddle = (vx, vy') }
-
-keyboardFunction (EventKey (SpecialKey KeyDown) _ _ _) game = if (player1Down game == False) then game { player1Down = True, player1Up = False } else game { player1Down = False } --game { player1Paddle = (vx, vy') }
-
-keyboardFunction (EventKey (Char 'w') _ _ _) game = if (player2Up game == False) then game { player2Up = True, player2Down = False } else game { player2Up = False } --game { player2Paddle = (vx, vy') }
-
-keyboardFunction (EventKey (Char 's') _ _ _) game = if (player2Down game == False) then game { player2Down = True, player2Up = False } else game { player2Down = False } --game { player2Paddle = (vx, vy') }
-
-keyboardFunction _ game = game
--- src/Main.hs
+++ src/Main.hs
@@ -1,9 +1,10 @@
 module Main(main) where
 
-import Graphics.Gloss -- to call animate here
+import Graphics.Gloss -- to call play here
 
 import Game -- game state logic
 import Display -- rendering of game state
+import Input -- keyboardFunction
 
 main :: IO ()
 main = play window background fps initialState render keyboardFunction update
